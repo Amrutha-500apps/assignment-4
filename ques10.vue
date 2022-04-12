3a722a18aedaa6c6b87523e003c82269ad312923
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-12 17:32:37 +0530

    Add files via upload

---
+++ ques10.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <button @click="functionhandle()">clickme</button>
+    </div>    
+</template>
+<script>
+export default {
+    name:"HII",
+    props:{
+        name:String,
+    },
+    methods:{
+        functionhandle(){
+            try{
+                this.count = this.count*5;
+                console.log("count",this.count);
+            }
+            catch{
+                console.log("error handled");
+            }
+        }
+    }
+}
+</script>
