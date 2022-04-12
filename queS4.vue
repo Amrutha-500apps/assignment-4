2c8f9b21e5f1e7b78fb3ec08c7a004a546ee2a36
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-12 17:30:08 +0530

    Add files via upload

---
+++ queS4.vue
@@ -0,0 +1,21 @@
+<template>
+<div id='app'>{{info}}
+<b-table striped hover :info="info" border=" "></b-table>
+</div>
+</template>
+<script>
+var axios = require("axios").default;
+export default{
+    name:"quesAKHUH",
+    data(){
+    return{
+    info:'',
+     }
+      },
+       mounted(){
+       axios
+      .get('https://api.publicapis.org/entries')
+      .then(response => (this.info = response))
+      }
+}
+</script>
