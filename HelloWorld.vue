1eb6c3f86711ab01ff5a26bbcb1ff2973ffa858a
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-12 17:32:49 +0530

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+<ques6 ref="message">{{message}}</ques6>
+<button @click="showAlert()">Click me</button>
+</div>
+</template>
+<script>
+import ques6 from '../components/ques6'
+export default {
+  component:{
+    ques6,
+  },
+      data(){
+      return {
+       name:"amrutha",
+       message:"hloo"
+     }
+   },
+    methods:{
+    showAlert(){
+     this.$refs.message.fun();
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
